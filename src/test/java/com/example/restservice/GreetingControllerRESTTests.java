f882dcf015e7320f4d876c5dc7bdfcac5d9a6b70
JimHinson <jim.hinson@live.com>
2020-07-04 16:44:01 -0400

    Fix directory structure

---
+++ src/test/java/com/example/restservice/GreetingControllerRESTTests.java
@@ -0,0 +1,12 @@
+package com.example.restservice;
+
+//import static io.restassured.
+//import static io.restassured.RestAssured.
+//io.restassured.RestAssured.*
+//io.restassured.matcher.RestAssuredMatchers.*
+//org.hamcrest.Matchers.*
+//io.restassured.module.jsv.JsonSchemaValidator.*;
+
+public class GreetingControllerRESTTests {
+
+}
